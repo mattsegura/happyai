898b0113c029dbe421709835d65b08fb18a3c35b
saadzarif <saad.zareef1212@gmail.com>
2025-11-09 05:48:01 +0500

    fixing nav

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-a959eb95'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.936bcnpe8io"
+    "revision": "0.9elikcobgu8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
